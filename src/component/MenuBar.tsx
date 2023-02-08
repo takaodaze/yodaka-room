c735079c33d938229f836a09f12b75107e2793a0
takaodaze <gin2798@gmail.com>
2023-02-09 03:05:25 +0900

    menu bar の実装

--- src/component/MenuBar.tsx
+++ src/component/MenuBar.tsx
@@ -2,17 +2,26 @@ import { paths } from "../router";
 
 export const MenuBar = () => {
     return (
-        <div className="bg-slate-900 w-fit h-screen flex flex-col gap-4 p-5">
-            {menu.map((m, idx) => (
-                <button
-                    key={`menu_${idx}`}
-                    className={
-                        "text-lg bg-slate-800 p-3 rounded-md hover:bg-slate-700 transition-all text-left"
-                    }
-                >
-                    {m.label}
-                </button>
-            ))}
+        <div className="bg-slate-900 w-fit h-screen p-5 flex flex-col gap-6">
+            <button
+                className={
+                    "text-3xl bg-slate-800 p-3 rounded-md hover:bg-slate-700 transition-all text-left"
+                }
+            >
+                ヨダカの部屋
+            </button>
+            <div className="w-fit flex flex-col gap-2">
+                {menu.map((m, idx) => (
+                    <button
+                        key={`menu_${idx}`}
+                        className={
+                            "text-lg bg-slate-800 p-3 rounded-md hover:bg-slate-700 transition-all text-left"
+                        }
+                    >
+                        {m.label}
+                    </button>
+                ))}
+            </div>
         </div>
     );
 };
@@ -23,19 +32,15 @@ type Menu = {
 };
 const menu: Menu[] = [
     {
-        label: "ヨダカの部屋",
+        label: "作品",
         path: paths.root,
     },
     {
-        label: "作品?",
+        label: "記事",
         path: paths.root,
     },
     {
-        label: "ここは？",
-        path: paths.root,
-    },
-    {
-        label: "あなたは?",
+        label: "プロフィール",
         path: paths.root,
     },
 ];
